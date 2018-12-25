890627d0c01b7ccc92b41dd6266ed91378b4082b
a.mikhaylov <aleksandr.mikhaylov@onetwotrip.com>
2018-12-26 02:54:03 +0300

    Реализации страницы загрузки

---
+++ src/pages/Loading/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+
+import Spinner from '../../components/Spinner';
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: Dimensions.get('window').height - 70,
+  },
+  wrap: {
+    width: Dimensions.get('window').width * 0.4,
+    height: Dimensions.get('window').width * 0.4,
+  },
+  titleWrap: {
+    marginTop: 100,
+  },
+  titleText: {
+    fontFamily: 'PT Sans',
+    fontSize: 22,
+    color: '#3275bd',
+  },
+});
+
+class LoadingPage extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func.isRequired,
+    }),
+  };
+
+  componentDidMount() {
+    const { navigation } = this.props;
+
+    setTimeout(() => navigation.navigate('Auth'), 2000);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.wrap}>
+          <Spinner />
+        </View>
+        <View style={styles.titleWrap}>
+          <Text style={styles.titleText}>Загрузка...</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default LoadingPage;
